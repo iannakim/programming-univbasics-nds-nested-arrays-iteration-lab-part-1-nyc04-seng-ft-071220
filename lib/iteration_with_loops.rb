64397ed6a62dd2b67eb48b246c5ae8d43083f5f3
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-20 17:58:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,13 @@
 def find_even_values(src)
-  src=[ [10, 11], [99, 50, 3, 4], [23, 41] ]
-  #
   count=0
-  even_array=[]
+  new_array=[]
 
   while count < src.length do
 
     inner_count=0
     while inner_count < src[count].length do
-      even_array << src[count][inner_count]
-      puts even_array.select(&:even?)
+      new_array << src[count][inner_count]
+      puts new_array.select(&:even?)
 
       inner_count +=1
   end
